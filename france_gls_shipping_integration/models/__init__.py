bc9d427a08089c7b18b7e4e2022fc3ac48666588
Vincent DUBREIL <vincent.dubreil@jjbconseil.com>
2022-03-07 14:39:15 +0100

    suppr other module + licence manifest

--- france_gls_shipping_integration/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import delivery_carrier
-from . import product_packaging
-from . import stock_picking
-from . import res_company
--- printnode_base/models/product_product.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2020 VentorTech OU
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
-
-from odoo import models
-
-
-class ProductProduct(models.Model):
-    _name = 'product.product'
-    _inherit = ['product.product', 'multi.print.mixin']
